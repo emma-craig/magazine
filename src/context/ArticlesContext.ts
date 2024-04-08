53e239064c1b0e51f3dbcf84ec4b12bea4bf911a
Emma Craig <emma.craig@getflowbox.com>
2024-04-08 17:12:22 +0200

    add server

--- src/context/ArticlesContext.ts
+++
@@ -1,7 +0,0 @@
-import  { createContext} from 'react'
-
-export const ArticlesContext = createContext<any | null>(null)
-
-
-
-export default ArticlesContext
--- src/hooks/useFetchArticles.js
+++
@@ -1,8 +0,0 @@
-import { articles } from './../mocks/article-list';
-
-const useFetchArticles = () => {
-  const data = articles;
-  return { data };
-};
-
-export default useFetchArticles;
