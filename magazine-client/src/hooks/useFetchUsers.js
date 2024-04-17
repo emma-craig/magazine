8de8797721a9e309cde44e4dd3f02e708c828a8c
Emma Craig <62936764+emma-craig@users.noreply.github.com>
2024-04-17 14:21:23 +0200

    Update useFetchUsers.js

--- magazine-client/src/hooks/useFetchUsers.js
+++ magazine-client/src/hooks/useFetchUsers.js
@@ -11,7 +11,7 @@ const useFetchUsers = () => {
       try {
         const response =
           // (await fetch('http://localhost:5000/users')) ;
-          await fetch('http://the-magazine-back-end.vercel.app/users');
+          await fetch('https://the-magazine-back-end.vercel.app/users');
 
         const user = await response.json();
         setUser(user);
