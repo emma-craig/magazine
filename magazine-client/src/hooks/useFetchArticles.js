2bb83c655e75719a4ab78d93debb94583fa888e7
Emma Craig <62936764+emma-craig@users.noreply.github.com>
2024-04-17 14:14:25 +0200

    Update useFetchArticles.js

--- magazine-client/src/hooks/useFetchArticles.js
+++ magazine-client/src/hooks/useFetchArticles.js
@@ -10,7 +10,8 @@ const useFetchArticles = () => {
       try {
         const response =
           // (await fetch('http://localhost:5000/articles')) ;
-          await fetch('http://the-magazine-back-hl8u3zu25-emmas-projects-a7ccc856.vercel.app/articles');
+          await fetch('http://the-magazine-back-end.vercel.app
+/articles');
           console.log(process.env.NODE_ENV_URL);
         const data = await response.json();
         setData(data);
