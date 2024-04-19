16eab16db2b02b9e927d97e77015a5b1ef6fcba4
Emma Craig <emma.craig@getflowbox.com>
2024-04-19 16:29:01 +0200

    remove trailing slash

--- client/src/hooks/useFetchUsers.js
+++ client/src/hooks/useFetchUsers.js
@@ -4,21 +4,21 @@ const useFetchUsers = () => {
   const [user, setUser] = useState({});
   const [error, setError] = useState();
   const [loading, setLoading] = useState(false);
-  const domain = process.env.NODE_ENV === 'production' ? 'https://monthly-elicia-pepper-magazine.koyeb.app/' : 'http://localhost:5000'
+  const domain =
+    process.env.NODE_ENV === 'production'
+      ? 'https://monthly-elicia-pepper-magazine.koyeb.app'
+      : 'http://localhost:5000';
 
   useEffect(() => {
     const fetchUsers = async () => {
       setLoading(true);
       try {
-        const response =
-          await fetch(`${domain}/users`, {
-
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-  
-            },
-          });
+        const response = await fetch(`${domain}/users`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
 
         const user = await response.json();
         setUser(user);
