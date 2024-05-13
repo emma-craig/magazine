b3b227f3a3fa998dd23f3bbd6a6fa0051c33bd81
Emma Craig <emma.craig@getflowbox.com>
2024-05-13 18:53:19 +0200

    update link

--- client/src/components/Title.tsx
+++ client/src/components/Title.tsx
@@ -3,6 +3,6 @@ import React from 'react'
 
 export default function Title() {
   return (
-    <Box sx={{display: 'flex', py: '2rem'}} bgcolor= '#f0f0f0' justifyContent='center' alignContent='center'><img src='/assets/pepper_title.png' alt='pepper magazine title' /></Box>
+    <Box sx={{display: 'flex', py: '2rem'}} bgcolor= '#f0f0f0' justifyContent='center' alignContent='center'><img src='/pepper_title.png' alt='pepper magazine title' /></Box>
   )
 }
