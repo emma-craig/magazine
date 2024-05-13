709eb0e9829bb22154cefe4195f896dafe7541bc
Emma Craig <emma.craig@getflowbox.com>
2024-05-13 18:10:58 +0200

    restyle header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,199 +1,62 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
+
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import { Link } from 'react-router-dom';
+
 import getCategories from '../utils/getCategories';
-import { useContext } from 'react';
+import {  useContext } from 'react';
 import ArticlesContext from '../contexts/ArticlesContext';
 import Stack from '@mui/material/Stack';
-import  AuthContext  from '../contexts/AuthContext';
-
-const settings = ['Login', 'Dashboard'];
+import { Link, useLocation } from 'react-router-dom';
+import { Tab, Tabs } from '@mui/material';
 
 const Header = () => {
   const data = useContext(ArticlesContext);
-  const currentUser = useContext(AuthContext) ?? undefined
   const categories = data && getCategories(data);
+  const { pathname } = useLocation();
 
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    null
-  );
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null
-  );
-
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
+const selectedCategory = () => {
+  if (pathname === '/') return 'Beauty'
+  return (
+    pathname.substring(1).charAt(0).toUpperCase() + pathname.substring(1).slice(1)
+  )
+}
   return (
     categories && (
-      <Stack>
-        <AppBar position="static">
-          <Container maxWidth="xl">
-            <Toolbar disableGutters>
-              <Typography
-                variant="h6"
-                noWrap
-                component="a"
-                href=""
-                sx={{
-                  mr: 2,
-                  display: { xs: 'none', md: 'flex' },
-                  fontFamily: 'monospace',
-                  fontWeight: 700,
-                  letterSpacing: '.3rem',
-                  color: 'inherit',
-                  textDecoration: 'none',
-                }}>
-                PEPPER
-              </Typography>
-
-              <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                <IconButton
-                  size="large"
-                  aria-label="account of current user"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  onClick={handleOpenNavMenu}
-                  color="inherit">
-                  <MenuIcon />
-                </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorElNav}
-                  anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
-                  }}
-                  open={Boolean(anchorElNav)}
-                  onClose={handleCloseNavMenu}
-                  sx={{
-                    display: { xs: 'block', md: 'none' },
-                  }}>
-                  {categories.map((category: any, index: number) => (
-                    <Link
-                      to={`/${category}`}
-                      style={{ textDecoration: 'none' }}
-                      key={index}>
-                      <MenuItem onClick={handleCloseNavMenu}>
-                        <Typography textAlign="center">{category}</Typography>
-                      </MenuItem>
-                    </Link>
-                  ))}
-                </Menu>
-              </Box>
-              <Typography
-                variant="h5"
-                noWrap
-                component="a"
-                href="#app-bar-with-responsive-menu"
-                sx={{
-                  mr: 2,
-                  display: { xs: 'flex', md: 'none' },
-                  flexGrow: 1,
-                  fontFamily: 'monospace',
-                  fontWeight: 700,
-                  letterSpacing: '.3rem',
-                  color: 'inherit',
-                  textDecoration: 'none',
-                }}>
-                PEPPER
-              </Typography>
-              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                {categories.map((category: any, index: number) => {
-                  return (
-                    <Link
-                      to={`/${category}`}
-                      style={{ textDecoration: 'none' }}
-                      key={index}>
-                      <Button
-                        onClick={handleCloseNavMenu}
-                        sx={{ my: 2, color: 'white', display: 'block' }}>
-                        {category}
-                      </Button>
-                    </Link>
-                  );
-                })}
-              </Box>
-
-              <Box sx={{ flexGrow: 0 }}>
-                <Tooltip title="Open user settings">
-                  <IconButton
-                    onClick={handleOpenUserMenu}
-                    sx={{ p: 0 }}>
-                    <Avatar
-                      sx={{ bgcolor: 'black' }}
-                      src="/broken-image.jpg"
-                    />
-                  </IconButton>
-                </Tooltip>
-                <Menu
-                  sx={{ mt: '45px' }}
-                  id="menu-appbar"
-                  anchorEl={anchorElUser}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  open={Boolean(anchorElUser)}
-                  onClose={handleCloseUserMenu}>
-                  {settings.map((setting, index) => (
-                    <Link
-                      to={`/${setting}`.toLowerCase()}
-                      style={{ textDecoration: 'none' }}
-                      key={index}>
-                      <MenuItem onClick={handleCloseUserMenu}>
-                        <Typography textAlign="center">{setting}</Typography>
-                      </MenuItem>
-                    </Link>
-                  ))}
-                </Menu>{' '}
-                {currentUser && (
-                  <Typography
-                    sx={{
-                      display: 'flex',
-                      justifyContent: 'flex-end',
-                      variant: 'caption',
-                    }}>
-                    {currentUser}
-                  </Typography>
-                )}
-              </Box>
-            </Toolbar>
-          </Container>
-        </AppBar>
+      <Stack flexDirection="column">
+        <Tabs
+          value={pathname === '/' ? '/beauty' : pathname}
+          centered
+          sx={{
+            '.MuiButtonBase-root': {
+              '&.Mui-selected': {
+                color: 'red',
+              },
+            },
+            '& .MuiTabs-indicator': { display: 'none' },
+          }}
+          >
+          {categories.map((category: any, index: number) => (
+            <Tab
+              component={Link}
+              to={`/${category}`}
+              style={{
+                textDecoration: 'none',
+                padding: '0.5rem 1.5rem',
+              }}
+              key={index}
+              label={category.toUpperCase()}
+              value={`/${category}`}
+            />
+          ))}
+        </Tabs>
+        <Box
+          sx={{ color: 'white', bgcolor: 'black' }}
+          display="flex"
+          justifyContent="center"
+          py="1rem">
+          <Typography variant="h4">{selectedCategory()}</Typography>
+        </Box>
       </Stack>
     )
   );
---
+++ client/src/components/Title.tsx
@@ -0,0 +1,8 @@
+import { Box } from '@mui/system'
+import React from 'react'
+
+export default function Title() {
+  return (
+    <Box sx={{display: 'flex', py: '2rem'}} bgcolor= '#f0f0f0' justifyContent='center' alignContent='center'><img src='/assets/pepper_title.png' alt='pepper magazine title' /></Box>
+  )
+}
