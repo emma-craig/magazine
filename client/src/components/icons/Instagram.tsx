8cdfb96a15f93d7c8df731b28a249d8e6e717322
Emma Craig <emma.craig@getflowbox.com>
2024-05-29 13:32:16 +0200

    change social url link colours

--- client/src/components/icons/Instagram.tsx
+++ client/src/components/icons/Instagram.tsx
@@ -8,7 +8,7 @@ const Instagram = ({ ...props }) => {
         height="24"
         viewBox="0 0 24 24"
         fill="none"
-        stroke="currentColor"
+        stroke="white"
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
--- client/src/components/icons/Youtube.tsx
+++ client/src/components/icons/Youtube.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Youtube = ({ ...props }) => {
+  
     return (
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -8,7 +9,7 @@ const Youtube = ({ ...props }) => {
         height="24"
         viewBox="0 0 24 24"
         fill="none"
-        stroke="currentColor"
+        stroke="white"
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
